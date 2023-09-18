c9e82eb1d732d2a58e4d42345f4466481428fa51
JoJinyoung <jjojin7168@gmail.com>
2023-09-18 14:31:16 +0900

    Update moving

--- character_grass.py
+++ character_grass.py
@@ -8,6 +8,12 @@ character = load_image('character.png')
 grass.draw_now(400, 30)
 character.draw_now(400, 90)
 
-delay(5)
+x = 0
+while(x<800):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, 90)
+    x = x + 2
+    delay(0.01)
 
 close_canvas()
