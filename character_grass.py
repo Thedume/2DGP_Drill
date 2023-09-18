c73e0621785991a05a2a86687c5f0cdc63ea1333
JoJinyoung <jjojin7168@gmail.com>
2023-09-18 15:23:46 +0900

    draw circle

--- character_grass.py
+++ character_grass.py
@@ -12,41 +12,52 @@ character.draw_now(400, 90)
 x = 400
 y = 90
 
+a = 400
+b = 90
+r = 255
+
 while(True):
     while(x<800):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, y)
-    x = x + 2
-    delay(0.01)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x + 4
+        delay(0.01)
 
     while(y<600):
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, y)
-        y = y + 2
+        y = y + 4
         delay(0.01)
 
     while(x>0):
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, y)
-        x = x - 2
+        x = x - 4
         delay(0.01)
 
     while(y>90):
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, y)
-        y = y - 2
+        y = y - 4
         delay(0.01)
 
     while(x<400):
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, y)
-        x = x + 2
+        x = x + 4
         delay(0.01)
+
+    for t in range(0, 360):
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(a + r * math.sin(math.radians(t)), b + r * math.cos(math.radians(t)) + r)
+            
+            delay(0.01)
         
 
 close_canvas()
