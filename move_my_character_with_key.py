81c77c689da58e6212f58b3c079b4ebf2e5140c4
JoJinyoung <jjojin7168@gmail.com>
2023-09-26 12:23:03 +0900

    Change Sprite & Speed
    스프라이트 애니메이션 이미지와 속도 조절

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -3,7 +3,7 @@ from pico2d import *
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
-character = load_image('animation_sheet.png')
+character = load_image('character_animation.png')
 
 
 def handle_events():
@@ -38,29 +38,40 @@ def handle_events():
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 dirX, dirY = 0, 0
+
+frameList = [105, 85, 82, 82, 112, 95, 73, 75]
+widthList = [78, 62, 66, 72, 81, 57, 49, 62]
+
 frame = 0
 
+s = 0
+i = 0
 while running:
     clear_canvas()
 
+    if i == 8:
+        s = 0
+        i = 0
+
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100, 100, 100, x, y)
+    character.clip_draw(s, 0, widthList[frame], 100, x, y, 150, 150)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
 
     if 50 <= x <= TUK_WIDTH-50:
-        x += dirX * 10
+        x += dirX * 15
     else:
         if (50 > x and dirX == 1) or (x > TUK_WIDTH-50 and dirX == -1):
-            x += dirX * 10
+            x += dirX * 15
     if 50 <= y <= TUK_HEIGHT-50:
-        y += dirY * 10
+        y += dirY * 15
     else:
         if (50 > y and dirY == 1) or (y > TUK_HEIGHT-50 and dirY == -1):
-            y += dirY * 10
-
+            y += dirY * 15
 
     delay(0.05)
+    s += frameList[i]
+    i += 1
 
 close_canvas()
