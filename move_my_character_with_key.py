655bf6f853eef163e9331e114728725f3a46b637
JoJinyoung <jjojin7168@gmail.com>
2023-09-26 11:38:09 +0900

    Make backgound & character motion
    사진은 확인용으로 실습에 사용한 스프라이트 사용

---
+++ move_my_character_with_key.py
@@ -0,0 +1,21 @@
+from pico2d import *
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+
+running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+frame = 0
+
+while running:
+    clear_canvas()
+
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100, 100, 100, x, y)
+    update_canvas()
+
+    frame = (frame + 1) % 8
+
+close_canvas()
