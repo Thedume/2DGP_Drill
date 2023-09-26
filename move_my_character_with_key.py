235445c7c4bc0d3e4aadc4ecebcea5936143a09a
JoJinyoung <jjojin7168@gmail.com>
2023-09-26 11:42:07 +0900

    Add Escape
    종료키 제작

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,6 +5,18 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+                
+
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
@@ -15,7 +27,7 @@ while running:
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     character.clip_draw(frame * 100, 100, 100, 100, x, y)
     update_canvas()
-
+    handle_events()
     frame = (frame + 1) % 8
 
 close_canvas()
