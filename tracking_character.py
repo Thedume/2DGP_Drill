a5136731b21064a2e79e8293168859d5b709231d
JoJinyoung <jjojin7168@gmail.com>
2023-09-30 01:24:16 +0900

    update loop

--- tracking_character.py
+++ tracking_character.py
@@ -14,22 +14,28 @@ frame = 0
 running = True
 hide_cursor()
 
-x1, y1 = x, y
-x2, y2 = rand_x, rand_y
 
-for i in range(1, 100 + 1):
-    clear_canvas()
-    TUK_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
-    hand.draw_now(rand_x, rand_y)
-    character.clip_draw(100 * frame, 100, 100, 100, x, y)
-    update_canvas()
-    frame = (frame + 1) % 8
 
-    t = i / 100
-    x = (1 - t) * x1 + t * x2
-    y = (1 - t) * y1 + t * y2
+while running:
+    rand_x, rand_y = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
 
-    delay(0.01)
+    x1, y1 = x, y
+    x2, y2 = rand_x, rand_y
+
+    for i in range(1, 100 + 1):
+        clear_canvas()
+
+        TUK_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
+        hand.draw_now(rand_x, rand_y)
+        character.clip_draw(100 * frame, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+
+        t = i / 100
+        x = (1 - t) * x1 + t * x2
+        y = (1 - t) * y1 + t * y2
+
+        delay(0.05)
 
 
 close_canvas()
